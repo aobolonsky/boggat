690daf9f8302f4a6649efce61ffd9ede00ecdf36
kras174 <kras32a@gmail.com>
2020-11-19 12:44:57 +0500

    #owl-carusel added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,19 @@
-$(function () {
-    $.pixlayout({
-        clip: true,
-        src: "assets/images/allpage.png",
-        show: true,
-        center: true,
-        top: 0,
-        left: 0,
-        pervious: false
+import "./jquery.pixlayout";
+import "./owl.carousel";
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+
+    $(function () {
+        $.pixlayout({
+            clip: true,
+            src: "assets/images/allpage.png",
+            show: true,
+            center: true,
+            top: 0,
+            left: 0,
+            pervious: false
+        });
     });
 });
+
