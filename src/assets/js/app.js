715b7b00bfebe25fdaf979b4e8add082f4cd7eff
Артем Оболонский <artem@gkb6.local>
2020-11-25 15:51:21 +0500

    о боггат

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,16 +5,16 @@ import "./owl.navigation";
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel();
 
-    //   $(function () {
-    //       $.pixlayout({
-    //           clip: true,
-    //           src: "assets/images/allpage.png",
-    //           show: true,
-    //           center: true,
-    //           top: 0,
-    //           left: 0,
-    //           pervious: false
-    //       });
-    //   });
+       $(function () {
+           $.pixlayout({
+               clip: true,
+               src: "assets/images/allpage.png",
+               show: true,
+               center: true,
+               top: 0,
+               left: 0,
+               pervious: false
+           });
+       });
 });
 
